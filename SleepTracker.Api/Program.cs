d6bffa1cf9ba7490e6015462b4a396198f7cab5c
Lepros311 <andy.strickland@gmail.com>
2025-11-13 19:46:40 -0500

    Add packages and Program.cs boilerplate

--- SleepTracker.Api/Program.cs
+++ SleepTracker.Api/Program.cs
@@ -1,22 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<SleepTrackerDbContext>(opt => opt.UseSqlServer
+    (builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ISleepRepository, SleepRepository>();
+builder.Services.AddScoped<ISleepService, SleepService>();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<SleepTrackerDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
