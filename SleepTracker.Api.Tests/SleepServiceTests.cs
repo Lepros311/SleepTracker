f66b943d2c4a3b718ab7db4e35596e3caea474ff
Lepros311 <andy.strickland@gmail.com>
2025-11-24 19:44:09 -0500

    Add SleepServiceTests

---
+++ SleepTracker.Api.Tests/SleepServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using SleepTracker.Api.Models;
+using SleepTracker.Api.Repositories;
+using SleepTracker.Api.Responses;
+using SleepTracker.Api.Services;
+
+namespace SleepTracker.Api.Tests;
+
+public class SleepServiceTests
+{
+    private Mock<ISleepRepository> _mockRepository;
+    private SleepService _service;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockRepository = new Mock<ISleepRepository>();
+        _service = new SleepService(_mockRepository.Object);
+    }
+
+    [TestMethod]
+    public async Task GetPagedSleeps_ReturnsSuccess_WhenRepositoryReturnsData()
+    {
+        // Arrange
+        var paginationParams = new PaginationParams { Page = 1, PageSize = 10 };
+        var sleepData = new List<Sleep>
+        {
+            new Sleep { Id = 1 }
+        };
+
+        var repositoryResponse = PagedResponse<List<Sleep>>.Success(sleepData, 1, 10, sleepData.Count);
+
+        _mockRepository.Setup(r => r.GetPagedSleeps(paginationParams)).ReturnsAsync(repositoryResponse);
+
+        // Act
+        var result = await _service.GetPagedSleeps(paginationParams);
+
+        // Assert
+        Assert.AreEqual(ResponseStatus.Success, result.Status);
+        Assert.AreEqual(1, result.Data.Count);
+        Assert.AreEqual(1, result.PageNumber);
+        Assert.AreEqual(10, result.PageSize);
+    }
+
+    [TestMethod]
+    public async Task GetPagedSleeps_ReturnsFail_WhenRepositoryThrows()
+    {
+        // Arrange
+        var paginationParams = new PaginationParams { Page = 1, PageSize = 10 };
+
+        _mockRepository.Setup(r => r.GetPagedSleeps(paginationParams)).ThrowsAsync(new System.Exception("DB error"));
+
+        // Act
+        var result = await _service.GetPagedSleeps(paginationParams);
+
+        // Assert 
+        Assert.AreEqual(ResponseStatus.Fail, result.Status);
+        Assert.AreEqual("DB error", result.Message);
+    }
+}
