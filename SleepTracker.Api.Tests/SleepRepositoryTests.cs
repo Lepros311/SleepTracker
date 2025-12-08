f6ba3c20539e58abd9a5d9e8bc9b2eb7bc0c125a
Lepros311 <andy.strickland@gmail.com>
2025-12-07 20:06:37 -0500

    Add test for pagination

--- SleepTracker.Api.Tests/SleepRepositoryTests.cs
+++ SleepTracker.Api.Tests/SleepRepositoryTests.cs
@@ -48,6 +48,24 @@ public class SleepRepositoryTests
     }
 
     [TestMethod]
+    public async Task GetPagedSleeps_ReturnsCorrectRecords_OnSecondPage()
+    {
+        // Arrange
+        var paginationParams = new PaginationParams { Page = 2, PageSize = 1 };
+
+        // Act
+        var result = await _repository.GetPagedSleeps(paginationParams);
+
+        // Assert
+        Assert.AreEqual(ResponseStatus.Success, result.Status);
+        Assert.AreEqual(1, result.Data.Count);
+        Assert.AreEqual(2, result.PageNumber);
+        Assert.AreEqual(1, result.PageSize);
+        Assert.AreEqual(2, result.TotalRecords);
+        Assert.AreEqual(1, result.Data.First().Id);
+    }
+
+    [TestMethod]
     public async Task GetPagedSleeps_ReturnsFail_WhenDbThrows()
     {
         // Arrange
